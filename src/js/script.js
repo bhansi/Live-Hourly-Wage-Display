5901c979f1cb075337b6b9c3fafafbce5c8ca362
Baljot Hansi <baljotshansi@gmail.com>
2024-03-16 13:40:18 -0400

    Added milliseconds to time display. USD and CAD displays working. Minor format changes. Code refactored.

--- src/js/script.js
+++ src/js/script.js
@@ -13,26 +13,64 @@ let btn_pause = document.getElementById("btn-pause");
 let btn_play = document.getElementById("btn-play");
 
 let interval;
+let start_time;
+let elapsed_time;
 
-input_wage.addEventListener("change", () => {
+let wage_usd;
+let wage_cad;
+let earnings_usd;
+
+function startInterval() {
+  start_time = new Date().getTime();
+  interval = setInterval(updateInterval, 1);
+}
+
+function updateInterval() {
+  updateOutputTime();
+  updateOutputUSD();
+  updateOutputCAD();
+}
+
+function updateOutputTime() {
+  let current_time = new Date().getTime();
+  elapsed_time = current_time - start_time;
+
+  let milliseconds = elapsed_time % 1000;
+  let seconds = Math.floor(elapsed_time / 1000) % 60;
+  let minutes = Math.floor(elapsed_time / 1000 / 60) % 60;
+  let hours = Math.floor(elapsed_time / 1000 / 60 / 60);
+
+  output_time.innerText = leadingZero(hours) + ":" + leadingZero(minutes) + ":" + leadingZero(seconds) + "." + leadingZeros(milliseconds);
+}
+
+function updateOutputUSD() {
+  earnings_usd = wage_usd * (elapsed_time / 1000 / 60 / 60);
+  output_usd.innerText = "USD: $" + (Math.round(earnings_usd * 100) / 100).toFixed(2);
+}
+
+function updateOutputCAD() {
+  let earnings_cad = earnings_usd * parseFloat(input_currency.value);
+  output_cad.innerText = "CAD: $" + (Math.round(earnings_cad * 100) / 100).toFixed(2);
+}
+
+function leadingZero(number) {
+  return (number < 10 ? "0" : "") + number;
+}
+
+function leadingZeros(number) {
+  return (number < 10 ? "00" : number < 100 ? "0" : "") + number;
+}
+
+function inputFieldHandler() {
   if(input_wage.value && input_currency.value) {
     btn_play.disabled = false;
   }
   else {
     btn_play.disabled = true;
   }
-});
+}
 
-input_currency.addEventListener("change", () => {
-  if(input_wage.value && input_currency.value) {
-    btn_play.disabled = false;
-  }
-  else {
-    btn_play.disabled = true;
-  }
-});
-
-btn_play.addEventListener("click", () => {
+function playButtonHandler() {
   btn_play.disabled = true;
   btn_pause.disabled = false;
   btn_stop.disabled = false;
@@ -44,26 +82,13 @@ btn_play.addEventListener("click", () => {
   output_usd.classList.remove("d-none");
   output_cad.classList.remove("d-none");
 
-  let wage_usd = parseFloat(input_wage.value);
-  let wage_cad = parseFloat(input_currency.value) * wage_usd;
+  wage_usd = parseFloat(input_wage.value);
+  wage_cad = parseFloat(input_currency.value) * wage_usd;
 
-  let start_time = new Date().getTime();
-  let counter = 0;
-
-  interval = setInterval(
-    () => {
-      var current_time = new Date().getTime();
-      var elapsed_time = current_time - start_time;
-      console.log(Math.floor(elapsed_time / 1000));
-      var seconds = Math.floor(elapsed_time / 1000) % 60;
-      var minutes = Math.floor(elapsed_time / 1000 / 60) % 60;
-      var hours = Math.floor(elapsed_time / 1000 / 60 / 60);
-      output_time.innerText = leadingZero(hours) + ":" + leadingZero(minutes) + ":" + leadingZero(seconds);
-    },
-    1000
-  );
-});
-
-function leadingZero(number) {
-  return (number < 10 ? "0" : "") + number;
+  startInterval();
 }
+
+input_wage.addEventListener("change", inputFieldHandler);
+input_currency.addEventListener("change", inputFieldHandler);
+
+btn_play.addEventListener("click", playButtonHandler);
