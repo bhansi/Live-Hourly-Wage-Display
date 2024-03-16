d40038dd99101c3ec1bbca1debcb8355907a836e
Baljot Hansi <baljotshansi@gmail.com>
2024-03-16 16:53:33 -0400

    Completed functionality and added flag images.

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,25 @@
 let input_wage = document.getElementById("input-wage");
 let input_currency = document.getElementById("input-currency");
 
-let inputgroup_wage = document.getElementById("inputgroup-wage");
-let inputgroup_currency = document.getElementById("inputgroup-currency");
-
 let output_time = document.getElementById("output-time");
 let output_usd = document.getElementById("output-usd");
 let output_cad = document.getElementById("output-cad");
 
-let btn_stop = document.getElementById("btn-stop");
+let btn_start = document.getElementById("btn-start");
 let btn_pause = document.getElementById("btn-pause");
-let btn_play = document.getElementById("btn-play");
+let btn_reset = document.getElementById("btn-reset");
 
 let interval;
 let start_time;
 let elapsed_time;
+let paused_time = 0;
 
 let wage_usd;
 let wage_cad;
 let earnings_usd;
 
 function startInterval() {
-  start_time = new Date().getTime();
+  start_time = new Date().getTime() - paused_time;
   interval = setInterval(updateInterval, 1);
 }
 
@@ -29,6 +27,12 @@ function updateInterval() {
   updateOutputTime();
   updateOutputUSD();
   updateOutputCAD();
+}
+
+function stopInterval() {
+  clearInterval(interval);
+  paused_time = new Date().getTime() - start_time;
+  interval = null;
 }
 
 function updateOutputTime() {
@@ -45,12 +49,12 @@ function updateOutputTime() {
 
 function updateOutputUSD() {
   earnings_usd = wage_usd * (elapsed_time / 1000 / 60 / 60);
-  output_usd.innerText = "USD: $" + (Math.round(earnings_usd * 100) / 100).toFixed(2);
+  output_usd.innerText = "$" + (Math.round(earnings_usd * 100) / 100).toFixed(2);
 }
 
 function updateOutputCAD() {
   let earnings_cad = earnings_usd * parseFloat(input_currency.value);
-  output_cad.innerText = "CAD: $" + (Math.round(earnings_cad * 100) / 100).toFixed(2);
+  output_cad.innerText = "$" + (Math.round(earnings_cad * 100) / 100).toFixed(2);
 }
 
 function leadingZero(number) {
@@ -61,26 +65,21 @@ function leadingZeros(number) {
   return (number < 10 ? "00" : number < 100 ? "0" : "") + number;
 }
 
-function inputFieldHandler() {
-  if(input_wage.value && input_currency.value) {
-    btn_play.disabled = false;
-  }
-  else {
-    btn_play.disabled = true;
+function inputFieldHandler(e) {
+  if((e.target === input_wage || e.target === input_currency) && (!e.target.value.isNaN)) {
+    if(input_wage.value && input_currency.value) {
+      btn_start.disabled = false;
+    }
+    else {
+      btn_start.disabled = true;
+    }
   }
 }
 
-function playButtonHandler() {
-  btn_play.disabled = true;
+function startButtonHandler() {
+  btn_start.disabled = true;
   btn_pause.disabled = false;
-  btn_stop.disabled = false;
-
-  inputgroup_wage.classList.add("d-none");
-  inputgroup_currency.classList.add("d-none");
-
-  output_time.classList.remove("d-none");
-  output_usd.classList.remove("d-none");
-  output_cad.classList.remove("d-none");
+  btn_reset.disabled = false;
 
   wage_usd = parseFloat(input_wage.value);
   wage_cad = parseFloat(input_currency.value) * wage_usd;
@@ -88,7 +87,29 @@ function playButtonHandler() {
   startInterval();
 }
 
-input_wage.addEventListener("change", inputFieldHandler);
-input_currency.addEventListener("change", inputFieldHandler);
+function pauseButtonHandler() {
+  btn_start.disabled = false;
+  btn_pause.disabled = true;
+  btn_reset.disabled = false;
 
-btn_play.addEventListener("click", playButtonHandler);
+  stopInterval();
+}
+
+function resetButtonHandler() {
+  btn_start.disabled = true;
+  btn_pause.disabled = true;
+  btn_reset.disabled = true;
+
+  input_wage.value = "";
+  input_currency.value = "";
+
+  stopInterval();
+  paused_time = 0;
+}
+
+input_wage.addEventListener("keyup", inputFieldHandler);
+input_currency.addEventListener("keyup", inputFieldHandler);
+
+btn_start.addEventListener("click", startButtonHandler);
+btn_pause.addEventListener("click", pauseButtonHandler);
+btn_reset.addEventListener("click", resetButtonHandler);
